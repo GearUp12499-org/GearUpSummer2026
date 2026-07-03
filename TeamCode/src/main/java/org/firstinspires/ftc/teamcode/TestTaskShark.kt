c296b7ff79e4c11e56bde468dd7d327cef254529
elainew09 <elainewen09@gmail.com>
2026-07-03 15:24:12 -0700

    copy Miles's gradle stuff :)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTaskShark.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestTaskShark.kt
@@ -2,16 +2,25 @@ package org.firstinspires.ftc.teamcode
 
 import android.util.Log
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp
 import io.github.gearup12499.taskshark.FastScheduler
 import io.github.gearup12499.taskshark.prefabs.OneShot
 
-
+@TeleOp
 class TestTaskShark: LinearOpMode() {
 
 
     override fun runOpMode() {
         val sch = FastScheduler()
 
-        sch.add(OneShot{Log.i("test", "yay it works")})
+        waitForStart()
+
+        while (opModeIsActive()) {
+            sch.add(OneShot { Log.i("test", "yay it works") })
+
+            sch.tick()
+
+        }
+
     }
 }
